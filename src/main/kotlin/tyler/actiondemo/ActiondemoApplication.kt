366265614346d58ba021bc1b159916b0c8fd840d
Tyler Gabb <tylerjgabb@gmail.com>
2022-05-13 06:13:19 -0700

    publish

---
+++ src/main/kotlin/tyler/actiondemo/ActiondemoApplication.kt
@@ -0,0 +1,11 @@
+package tyler.actiondemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActiondemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActiondemoApplication>(*args)
+}
---
+++ src/test/kotlin/tyler/actiondemo/ActiondemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package tyler.actiondemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActiondemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
